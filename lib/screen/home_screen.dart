e9fc62cc3a2929d900cbb3e178da46077485a95d
Omar Falah <91734504+omarfalah99@users.noreply.github.com>
2022-03-28 18:37:52 +0300

    Adding drawers

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -18,91 +18,69 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    final key = GlobalKey<SliderDrawerState>();
-
-    var isOpen = false;
-    void drawerFunctionality() {
-      if (!isOpen) {
-        key.currentState?.openSlider();
-        isOpen = true;
-      } else {
-        key.currentState?.closeSlider();
-        isOpen = false;
-      }
-    }
-
     return Scaffold(
-      // drawer: MainDrawer(),
-      body: SliderDrawer(
-        sliderOpenSize: MediaQuery.of(context).size.width * 0.58,
-        key: key,
-        appBar: AppBar(
-          backgroundColor: Colors.blue[900],
-          leading: IconButton(
-            icon: Icon(Icons.menu),
-            onPressed: drawerFunctionality,
-          ),
-          title: const Icon(Icons.location_on),
-          centerTitle: true,
-          actions: const [
-            Icon(Icons.search_outlined),
-          ],
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(10),
-              bottomRight: Radius.circular(10),
-            ),
+      drawer: MainDrawer(),
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: const Icon(Icons.location_on),
+        centerTitle: true,
+        actions: const [
+          Icon(Icons.search_outlined),
+        ],
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(10),
+            bottomRight: Radius.circular(10),
           ),
         ),
-        slider: const MainDrawer(),
-        child: Column(
-          children: [
-            Container(
-              width: double.infinity,
-              height: 110,
-              child: Image.asset(
-                'assets/engineer.jpg',
-                fit: BoxFit.cover,
-              ),
+      ),
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 110,
+            child: Image.asset(
+              'assets/engineer.jpg',
+              fit: BoxFit.cover,
             ),
-            RowText('Feature', 'More'),
-            Container(
-              height: 100,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) {
-                  return Users(WORKER_DATA[index].name, WORKER_DATA[index].work,
-                      WORKER_DATA[index].phone, WORKER_DATA[index].id);
-                },
-                itemCount: WORKER_DATA.length,
-              ),
+          ),
+          RowText('Feature', 'More'),
+          Container(
+            height: 100,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return Users(WORKER_DATA[index].name, WORKER_DATA[index].work,
+                    WORKER_DATA[index].phone, WORKER_DATA[index].id);
+              },
+              itemCount: WORKER_DATA.length,
             ),
-            RowText('Best', 'More'),
-            Container(
-              height: 100,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) {
-                  return Users(WORKER_DATA[index].name, WORKER_DATA[index].work,
-                      WORKER_DATA[index].phone, WORKER_DATA[index].id);
-                },
-                itemCount: WORKER_DATA.length,
-              ),
+          ),
+          RowText('Best', 'More'),
+          Container(
+            height: 100,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return Users(WORKER_DATA[index].name, WORKER_DATA[index].work,
+                    WORKER_DATA[index].phone, WORKER_DATA[index].id);
+              },
+              itemCount: WORKER_DATA.length,
             ),
-            RowText('New', 'More'),
-            Container(
-              height: 100,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) {
-                  return Users(WORKER_DATA[index].name, WORKER_DATA[index].work,
-                      WORKER_DATA[index].phone, WORKER_DATA[index].id);
-                },
-                itemCount: WORKER_DATA.length,
-              ),
+          ),
+          RowText('New', 'More'),
+          Container(
+            height: 100,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return Users(WORKER_DATA[index].name, WORKER_DATA[index].work,
+                    WORKER_DATA[index].phone, WORKER_DATA[index].id);
+              },
+              itemCount: WORKER_DATA.length,
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
